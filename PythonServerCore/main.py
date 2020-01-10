523737ab8a48dc3a58d27bef04297f63e1110f37
Istiaque Hussain <istiaque_hussain@apple.com>
2020-01-10 16:38:47 +0530

    Second push

--- PythonServerCore/main.py
+++ PythonServerCore/main.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask as webServer
 from flask import render_template
+from flask import redirect, url_for, request
 import GPIOService as gpioservice
 
 def readServerConfig():
@@ -23,6 +24,26 @@ def renderSingleButtonLoayout():
 @app.route(serverCong["rootcontext"]+"/multibuttonlayout")
 def renderMutiButtonLoayout():
     return render_template("/multibuttonlayout.html",buttons=inputs)
+
+@app.route(serverCong["rootcontext"]+"/singlebuttonaction",methods = ['POST', 'GET'])
+def renderSingleButtonAction():
+    actionLeds=["D-1"]
+    if request.method == 'GET':
+        leds = request.args.get("ID")
+        actionLeds.append(leds)
+        #actionLeds=actionLeds.append(leds)
+        #actionLeds=actionLeds+[leds]
+        #print("Following LD will be on")
+        #print(actionLeds)
+        gpioservice.GPIO_OUT_Action(outputs,actionLeds,"ON")
+        return render_template("/singlebuttonlayout.html",buttons=outputs)
+    else:
+        leds = request.form["ID"]
+        actionLeds.append(leds)
+        gpioservice.GPIO_OUT_Action(outputs,actionLeds,"ON")
+        print("Following LD will be on")
+        print(leds)
+        return render_template("/singlebuttonlayout.html",buttons=outputs)
 
 
 @app.route(serverCong["rootcontext"]+"/welcome")
